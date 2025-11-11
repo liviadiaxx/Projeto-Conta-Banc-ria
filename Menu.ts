06170b26663262b8dde92caf9468f8f1e209b96e
Liviadiaxx <liviadiasxavier@hotmail.com>
2025-11-11 13:43:30 -0300

    Adicionei cores ao meu menu

--- Menu.ts
+++ Menu.ts
@@ -1,4 +1,5 @@
 import readlinesync = require("readline-sync");
+import { colors } from './src/util/Colors';
 
 export function main() {
 
@@ -6,7 +7,8 @@ export function main() {
 
     while (true) {
 
-        console.log("*****************************************************");
+        console.log(colors.bg.black, colors.fg.yellow, 
+                    "*****************************************************");
         console.log("                                                     ");
         console.log("                BANCO DO BRAZIL COM Z                ");
         console.log("                                                     ");
@@ -23,53 +25,74 @@ export function main() {
         console.log("            9 - Sair                                 ");
         console.log("                                                     ");
         console.log("*****************************************************");
-        console.log("                                                     ");
+        console.log("                                                     ", 
+        colors.reset);
 
         console.log("Entre com a opção desejada: ");
         opcao = readlinesync.questionInt("");
 
         if (opcao == 9) {
-            console.log("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+            console.log(colors.fg.greenstrong, 
+                "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
             sobre();
+            console.log(colors.reset, "");
             process.exit(0);
         }
 
         switch (opcao) {
             case 1:
-                console.log("\n\nCriar Conta\n\n");
-
+                console.log(colors.fg.whitestrong, 
+                    "\n\nCriar Conta\n\n", colors.reset);
+                
+                keyPress()
                 break;
             case 2:
-                console.log("\n\nListar todas as Contas\n\n");
+                console.log(colors.fg.whitestrong, 
+                    "\n\nListar todas as Contas\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 3:
-                console.log("\n\nConsultar dados da Conta - por número\n\n");
+                console.log(colors.fg.whitestrong, 
+                    "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 4:
-                console.log("\n\nAtualizar dados da Conta\n\n");
+                console.log(colors.fg.whitestrong, 
+                    "\n\nAtualizar dados da Conta\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 5:
-                console.log("\n\nApagar uma Conta\n\n");
+                console.log(colors.fg.whitestrong, 
+                    "\n\nApagar uma Conta\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 6:
-                console.log("\n\nSaque\n\n");
+                console.log(colors.fg.whitestrong, 
+                    "\n\nSaque\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 7:
-                console.log("\n\nDepósito\n\n");
+                console.log(colors.fg.whitestrong, 
+                    "\n\nDepósito\n\n", colors.reset);
 
+                keyPress()
                 break;
             case 8:
-                console.log("\n\nTransferência entre Contas\n\n");
+                console.log(colors.fg.whitestrong, 
+                    "\n\nTransferência entre Contas\n\n", colors.reset);
 
+                keyPress()
                 break;
             default:
-                console.log("\nOpção Inválida!\n");
+                console.log(colors.fg.whitestrong, 
+                    "\nOpção Inválida!\n", colors.reset);
 
+                keyPress()
                 break;
         }
     }
@@ -77,13 +100,18 @@ export function main() {
 }
 
 /* Função com os dados da pessoa desenvolvedora */
-
-export function sobre(): void {
+function sobre(): void {
     console.log("\n*****************************************************");
     console.log("Projeto Desenvolvido por: ");
-    console.log("Generation Brasil - generation@generation.org");
+    console.log("Lívia Dias Xavier - livia.dias3012@gmail.com");
     console.log("github.com/conteudoGeneration");
     console.log("*****************************************************");
+}
+
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
 }
 
 main();
