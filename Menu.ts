cf020f67660ae7a858589f05f1289aaafaea0ddc
Liviadiaxx <liviadiasxavier@hotmail.com>
2025-11-12 09:13:14 -0300

    Adicionei classes

--- Menu.ts
+++ Menu.ts
@@ -1,18 +1,36 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
 import { Conta } from "./src/model/conta";
+import { ContaCorrente } from "./src/model/contaCorrente";
+import { ContaPoupanca } from "./src/model/contaPoupanca";
 
 export function main() {
 
     let opcao: number;
-
-    // Objeto da Classe Conta (Teste)
+    
+   // Objeto da Classe Conta (Teste)
     const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
     conta.visualizar();
     conta.sacar(10500);
     conta.visualizar();
     conta.depositar(5000);
     conta.visualizar();
+
+   // Objeto da Classe ContaCorrente (Teste)
+    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
+
+    // Objeto da Classe ContaPoupanca (teste)
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
 
     while (true) {
 
@@ -34,8 +52,7 @@ export function main() {
         console.log("            9 - Sair                                 ");
         console.log("                                                     ");
         console.log("*****************************************************");
-        console.log("                                                     ", 
-        colors.reset);
+        console.log("                                                     ", colors.reset);
 
         console.log("Entre com a opção desejada: ");
         opcao = readlinesync.questionInt("");
@@ -102,8 +119,8 @@ export function main() {
 function sobre(): void {
     console.log("\n*****************************************************");
     console.log("Projeto Desenvolvido por: ");
-    console.log("Lívia Dias Xavier, livia.dias3012@gmail.com");
-    console.log("/github.com/liviadiaxx");
+    console.log("Livia Dias Xavier - Livia.dias3012@gmail.com");
+    console.log("github.com/liviadiaxx");
     console.log("*****************************************************");
 }
 
